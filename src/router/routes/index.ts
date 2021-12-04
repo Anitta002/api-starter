823a892d32af14a2bc76d34a5605555189f6f52c
Anitta002 <84449344+Anitta002@users.noreply.github.com>
2021-12-04 22:12:22 +0530

    Update index.ts

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 /** Returns base router. */
+
 export default (path: string, app: Router): Router => {
   const router = Router();
 
   app.use(path, router);
+  
 
   return router;
 };
