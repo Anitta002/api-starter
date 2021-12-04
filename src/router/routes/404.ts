b25fb71ca39a609c3e578101aa6cac7e36c5fb04
Anitta002 <84449344+Anitta002@users.noreply.github.com>
2021-12-04 22:17:06 +0530

    Update 404.ts

--- src/router/routes/404.ts
+++ src/router/routes/404.ts
@@ -5,10 +5,12 @@ import { setError } from '../../helpers/error';
 /** Sends 404 for invalid route requests
  *  Works with every HTTP methods
  **/
-export default (path: string, apiRouter: Router): void => {
+export default (path: string, apiRouter: Router): void =>
+{
   const route = router(path, apiRouter);
 
-  route.use((_: Request, res: Response) => {
+  route.use((_: Request, res: Response) =>
+            {
     const err = setError(404);
 
     res.status(404).send(err);
