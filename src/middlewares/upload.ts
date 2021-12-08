48bf938dcbaa0852200f6547024d5c6ea69f8e5c
Anitta002 <84449344+Anitta002@users.noreply.github.com>
2021-12-08 21:00:25 +0530

    Update upload.ts

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -3,11 +3,14 @@ import * as multer from 'multer';
 /** Returns middleware that allows uploading avatars. */
 const avatar = multer({
   limits: { fileSize: 5000000 },
-  fileFilter(_, file, callback) {
+  fileFilter(_, file, callback)
+  {
     if (
       file.originalname.match(/\.(jp[e]?|pn)g$/) &&
       file.mimetype.match(/^(image\/)(jp[e]?|pn)g$/)
-    ) {
+    )
+    
+    {
       callback(undefined, true);
       return;
     }
